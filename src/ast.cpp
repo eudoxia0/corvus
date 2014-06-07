702bf8d16def045cc6f92023d9d04911b1d19f7e
Fernando Borretti <eudoxiahp@gmail.com>
2014-06-06 21:15:25 -0300

    Add implementation of AST push function

--- src/ast.cpp
+++ src/ast.cpp
@@ -16,3 +16,12 @@ SExp* cons(SExp* first, SExp* rest) {
   rest(sexp)  = rest;
   return sexp;
 }
+
+void push(SExp* list, SExp* obj) {
+  assert(listp(list));
+  SExp* p = list;
+  while(rest(p) != NULL) {
+    p = rest(p);
+  }
+  rest(p) = obj;
+}
