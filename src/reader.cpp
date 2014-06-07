ac567109818940886d323c4ab8963aec180615cb
Fernando Borretti <eudoxiahp@gmail.com>
2014-06-07 20:46:52 -0300

    Add fairly complete readStream implementation

--- src/reader.cpp
+++ src/reader.cpp
@@ -17,3 +17,38 @@ SExp* vectorToList(std::vector<SExp*> list) {
 bool completeToken(std::string tok_text) {
   return tok_text.length() > 0;
 }
+
+SExp* readStream(FILE* stream) {
+  std::string token_text;
+  char c = (char)getc(stream);
+  while(c != EOF) {
+    /* If c is a whitespace character, discard it and re-enter the loop, unless
+       we are reading a token, in which case the whitespace terminates it. */
+    if(isspace(c)) {
+      if(completeToken(token_text))
+        /* Return the complete token */
+        return makeSExp(token_text, IDENTIFIER);
+      else
+        continue;
+    }
+    /* If c is a dispatching or non-dispatching macro character, its associated
+       function is called */
+    /* Until I implement reader macros satisfactorily, we'll cheat and directly
+       implement the 'macros' for parentheses and quotes. */
+    if(c == '(') {
+      return readDelimitedSequence(stream, ')');
+    }
+    /* Any character that is not a macro character is a constituent character
+       of a token. At this point, a token begins to be accumulated */
+    token_text += c;
+    c = (char)getc(stream);
+    if(peekc(stream) == ')')
+      return makeSExp(token_text, IDENTIFIER);
+  }
+  /* End-of-file was reached */
+  if(completeToken(token_text))
+    return makeSExp(token_text, IDENTIFIER);
+  else
+    /* EOF was reached before a complete token was read */
+    return NULL;
+}
