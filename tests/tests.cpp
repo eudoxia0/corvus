f1c96cd1fac40f67b250ed73cdd7ca7315de39f4
Fernando Borretti <eudoxiahp@gmail.com>
2014-06-08 21:04:01 -0300

    Update tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,42 +1,41 @@
 #include "reader.hpp"
 #include "framework.cpp"
-
-SExp* a = makeSExp("a", IDENTIFIER);
-SExp* b = makeSExp("b", IDENTIFIER);
-SExp* c = makeSExp("c", IDENTIFIER);
-SExp* scons = NULL;
-SExp* mcons = NULL;
+#include <iostream>
 
 TEST(print_empty) {
   ASSERT(print(NULL) == "()");
 }
 
 TEST(print_atom) {
+  SExp* a = makeSExp("a", IDENTIFIER);
   ASSERT(print(a) == "a");
 }
 
 TEST(consing) {
-  scons = cons(a, NULL);
+  SExp* a = makeSExp("a", IDENTIFIER);
+  SExp* scons = cons(a, NULL);
   ASSERT(print(scons) == "(a)");
 }
 
 TEST(multiple_consing) {
-  mcons = cons(a, cons(b, cons(c, NULL)));
+  SExp* a = makeSExp("a", IDENTIFIER);
+  SExp* b = makeSExp("b", IDENTIFIER);
+  SExp* c = makeSExp("c", IDENTIFIER);
+  SExp* mcons = cons(a, cons(b, cons(c, NULL)));
   ASSERT(print(mcons) == "(a b c)");
 }
 
-TEST(free_atoms) {
-  freeSExp(a);
-  pass();
-  freeSExp(b);
-  pass();
-  freeSExp(c);
+TEST(free_atom) {
+  SExp* atom = makeSExp("test", IDENTIFIER);
+  freeSExp(atom);
   pass();
 }
 
-TEST(free_lists) {
-  freeSExp(scons);
-  pass();
+TEST(free_list) {
+  SExp* a = makeSExp("a", IDENTIFIER);
+  SExp* b = makeSExp("b", IDENTIFIER);
+  SExp* c = makeSExp("c", IDENTIFIER);
+  SExp* mcons = cons(a, cons(b, cons(c, NULL)));
   freeSExp(mcons);
   pass();
 }
@@ -46,8 +45,8 @@ SUITE(ast) {
   RUN_TEST(print_atom);
   RUN_TEST(consing);
   RUN_TEST(multiple_consing);
-  RUN_TEST(free_atoms);
-  RUN_TEST(free_lists);
+  RUN_TEST(free_atom);
+  RUN_TEST(free_list);
 }
 
 int main(int argc, char **argv) {
