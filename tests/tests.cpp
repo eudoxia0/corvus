c1864cb32a7b64a594e87828f2b991598dfdfbc0
Fernando Borretti <eudoxiahp@gmail.com>
2014-06-16 20:48:54 -0300

    Add more tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -31,11 +31,55 @@ TEST(multiple_consing) {
   pass();
 }
 
+TEST(concatenating) {
+  SExp* a = makeSExp("a", IDENTIFIER);
+  SExp* b = makeSExp("b", IDENTIFIER);
+  SExp* c = makeSExp("c", IDENTIFIER);
+  SExp* s1 = makeSExp("1", IDENTIFIER);
+  SExp* s2 = makeSExp("2", IDENTIFIER);
+  SExp* s3 = makeSExp("3", IDENTIFIER);
+  SExp* list1 = cons(a, cons(b, cons(c, NULL)));
+  SExp* list2 = cons(s1, cons(s2, cons(s3, NULL)));
+  SExp* list = concat(list1, list2);
+  ASSERT(print(list) == "(a b c 1 2 3)");
+  freeSExp(list);
+  pass();
+}
+
+TEST(reversing) {
+  SExp* a = makeSExp("a", IDENTIFIER);
+  SExp* b = makeSExp("b", IDENTIFIER);
+  SExp* c = makeSExp("c", IDENTIFIER);
+  SExp* list = cons(a, cons(b, cons(c, NULL)));
+  SExp* reversed = reverse(list);
+  ASSERT(print(reversed) == "(c b a)");
+  freeSExp(reversed);
+  pass();
+}
+
+TEST(nested_list) {
+  SExp* a = makeSExp("a", IDENTIFIER);
+  SExp* b = makeSExp("b", IDENTIFIER);
+  SExp* c = makeSExp("c", IDENTIFIER);
+  SExp* nest1 = cons(a, cons(b, cons(c, NULL)));
+  SExp* s1 = makeSExp("1", IDENTIFIER);
+  SExp* s2 = makeSExp("1", IDENTIFIER);
+  SExp* s3 = makeSExp("1", IDENTIFIER);
+  SExp* nest2 = cons(cons(s1, cons(s2, cons(s3, NULL))), NULL);
+  SExp* nest = cons(nest1, nest2);
+  ASSERT(print(nest) == "((a b c) (1 1 1))");
+  freeSExp(nest);
+  pass();
+}
+
 SUITE(ast) {
   RUN_TEST(print_empty);
   RUN_TEST(print_atom);
   RUN_TEST(consing);
   RUN_TEST(multiple_consing);
+  RUN_TEST(concatenating);
+  RUN_TEST(reversing);
+  RUN_TEST(nested_list);
 }
 
 TEST(read_atom) {
@@ -54,9 +98,23 @@ TEST(read_list) {
   fclose(file);
 }
 
+TEST(read_nested_lists) {
+  FILE* file = fopen("tests/input.3.txt", "r");
+  SExp* list = readStream(file);
+  ASSERT(print(list) == std::string("(((a) b) c)"));
+  freeSExp(list);
+  fclose(file);
+  file = fopen("tests/input.4.txt", "r");
+  list = readStream(file);
+  ASSERT(print(list) == std::string("(a (b (c)))"));
+  freeSExp(list);
+  fclose(file);
+}
+
 SUITE(reader) {
   RUN_TEST(read_atom);
   RUN_TEST(read_list);
+  RUN_TEST(read_nested_lists);
 }
 
 int main(int argc, char **argv) {
