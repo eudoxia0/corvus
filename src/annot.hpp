bbf8789f06ebfcad99d50409a87767741b81782c
Fernando Borretti <eudoxiahp@gmail.com>
2014-06-17 20:44:10 -0300

    Add some more node classes

--- src/annot.hpp
+++ src/annot.hpp
@@ -1,1 +1,33 @@
 /* Turn the AST into an annotated AST for function lifting. */
+#include "ast.hpp"
+#include <vector>
+#include <utility>
+
+/* Utility types */
+typedef std::vector<std::pair<const char*, SExp*> > Bindings;
+
+class AnnotAST { };
+
+class LetAST : public AnnotAST {
+  Bindings bindings;
+public:
+  LetAST(Bindings bind) : bindings(bind) {}
+};
+
+class LambdaAST : public AnnotAST {
+public:
+  Bindings arguments;
+  SExp* ret;
+  SExp* body;
+  LambdaAST(Bindings args, SExp* ret, SExp* body) :
+    arguments(args), ret(ret), body(body) {}
+};
+
+class FunctionAST : public LambdaAST {
+  const char* name;
+public:
+  FunctionAST(const char* name, Bindings args, SExp* ret, SExp* body):
+    LambdaAST(args, ret, body) {
+    this->name = name;
+  }
+};
