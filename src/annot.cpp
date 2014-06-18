3621c1742b96bb55a31d3071cb61c9454aee7290
Fernando Borretti <eudoxiahp@gmail.com>
2014-06-17 21:03:21 -0300

    Implement annotation for atoms

--- src/annot.cpp
+++ src/annot.cpp
@@ -1,1 +1,19 @@
 #include "annot.hpp"
+
+AnnotAST* annotateList(SExp* list) {
+  return NULL;
+}
+
+AnnotAST* annotate(SExp* sexp) {
+  if(sexp) {
+    if(listp(sexp)) {
+      return annotateList(sexp);
+    } else {
+      return new AtomAST(sexp->content.atom);
+    }
+  } else {
+    /* A null pointer represents the null form, which designates the unit
+       type. */
+    return NULL;
+  }
+}
