1cf5160e40429ff049306a32c76bd27702785525
Fernando Borretti <eudoxiahp@gmail.com>
2014-06-18 21:38:58 -0300

    Add width method, function pointer type

--- src/types.hpp
+++ src/types.hpp
@@ -21,6 +21,7 @@ struct Integer : Scalar {
   IntegerType type;
 public:
   Integer(IntegerType t) : type(t) { }
+  int width() { return (int)(this->type); }
 };
 
 enum FloatType { Half, Single, Double, Quad };
@@ -29,6 +30,7 @@ struct Float : Scalar {
   FloatType type;
 public:
   Float(FloatType t) : type(t) { }
+  int width() { return (int)(this->type); }
 };
 
 /* Aggregate types */
@@ -46,19 +48,28 @@ public:
   Tuple(std::vector<Type> tup_types) : types(tup_types) { }
 };
 
-struct RecordField {
+struct TypeField {
   char* name;
   Type type;
   char* docstring;
 public:
-  RecordField(char* n, Type t, char* doc=NULL) :
+  TypeField(char* n, Type t, char* doc=NULL) :
     name(n), type(t), docstring(doc) { }
 };
 
 struct Record : Aggregate {
-  std::vector<RecordField> fields;
+  std::vector<TypeField> fields;
 public:
-  Record(std::vector<RecordField> record_fields) : fields(record_fields) { }
+  Record(std::vector<TypeField> record_fields) : fields(record_fields) { }
+};
+
+/* Function pointers */
+struct Function : Type {
+  std::vector<TypeField> arg_types;
+  Type return_type;
+public:
+  Function(std::vector<TypeField> args, Type ret) :
+    arg_types(args), return_type(ret) { }
 };
 
 /* Pointers */
@@ -68,3 +79,15 @@ struct Pointer : Type {
 };
 
 /* Type constants */
+
+/*Type i1 = Integer(Bool);
+Type i8 = Integer(Byte);
+Type i16 = Integer(Short);
+Type i32 = Integer(Int32);
+Type i64 = Integer(Int64);
+Type i128 = Integer(Int128);
+
+Type HalfFloat = Float(Half);
+Type SingleFloat = Float(Single);
+Type DoubleFloat = Float(Double);
+Type QuadFloat = Float(Quad);*/
