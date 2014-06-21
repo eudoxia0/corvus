b263e2ca92bdc43c6bb908015eb3162943f5f707
Fernando Borretti <eudoxiahp@gmail.com>
2014-06-20 22:03:26 -0300

    Add lift-types.*pp

---
+++ src/lift-types.cpp
@@ -0,0 +1,1 @@
+#include "lift-types.hpp"
---
+++ src/lift-types.hpp
@@ -0,0 +1,7 @@
+/* Extract type definitions into a type environment */
+#include "types.hpp"
+
+/* The public interface to the type lifter: Takes an S-expression and returns a
+   type environment. Type definitions are replaced with the truth constant
+   in-place. */
+TypeEnv* extractTypes(SExp* sexp);
