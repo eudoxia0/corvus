76326dd4276f6f4049b3c44374e62dc064f3f341
Fernando Borretti <eudoxiahp@gmail.com>
2014-06-24 20:17:12 -0300

    Add some code to type lifter

--- src/lift-types.cpp
+++ src/lift-types.cpp
@@ -1,1 +1,16 @@
 #include "lift-types.hpp"
+
+void findTypes(SExp* sexp, TypeEnv* tenv) {
+  if(listp(sexp)) {
+    /* Is the first element of the list the identifier "type"?  If so, parse the
+       type definition. Otherwise, loop over the elements of the list, finding
+       their types. */
+  }
+  /* Atoms are not type definitions, so do nothing */
+}
+
+TypeEnv* extractTypes(SExp* sexp) {
+  TypeEnv* tenv = new TypeEnv;
+  findTypes(sexp, tenv);
+  return tenv;
+}
--- src/lift-types.hpp
+++ src/lift-types.hpp
@@ -2,6 +2,14 @@
 #include "ast.hpp"
 #include "types.hpp"
 
+/* Validate a type definition form */
+int isValid(SExp* form);
+
+/* A recursive function that does the actual extraction. Recurs over sexp,
+   replacing type definitions with truth constants and adding them to the type
+   environment. */
+void findTypes(SExp* sexp, TypeEnv* tenv);
+
 /* The public interface to the type lifter: Takes an S-expression and returns a
    type environment. Type definitions are replaced with the truth constant
    in-place. */
