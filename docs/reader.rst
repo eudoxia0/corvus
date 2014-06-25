9a9f62dc6b20a36457ccae663c7bab37b62c63b6
Fernando Borretti <eudoxiahp@gmail.com>
2014-06-25 19:53:51 -0300

    Turn the reader.hpp comment into rST and include it from reader.rst

--- docs/reader.rst
+++ docs/reader.rst
@@ -8,3 +8,7 @@ Reader Algorithm
 Corvus' reader algorithm is loosely based on Common Lisp's, as `described
 <http://www.lispworks.com/documentation/HyperSpec/Body/02_b.htm>`_ in the Common
 Lisp Specification.
+
+.. include:: ../src/reader.hpp
+             :start-line: 8
+             :end-line: 57
--- src/reader.hpp
+++ src/reader.hpp
@@ -4,57 +4,58 @@
 #include <vector>
 #include <algorithm>
 
-/* Reader Macros
+/*
+The readtable is essentially a table like this:
 
-   The readtable is essentially a table like this:
+=====  ==============
+Bytes  Macro Function
+=====  ==============
+;      readComment
+#[     readArrayLiteral
+#{     readRecordLiteral
+=====  ==============
 
-      |  Bytes | Macro function    |
-    --+--------+-------------------+
-    0 | //     | readComment       |
-    1 | #[     | readArrayLiteral  |
-    2 | #{     | readRecordLiteral |
+The readStream function reads bytes one at a time. The first byte it reads (At
+the beginning of the input stream, or when it's just finished a previous token)
+is searched in the readtable: If it's not in the first position of any entry,
+it's just a regular token. If it is, then the function sees its about to receive
+a reader macro.
 
-   The readStream function reads bytes one at a time. The first byte it reads
-   (At the beginning of the input stream, or when it's just finished a previous
-   token) is searched in the readtable: If it's not in the first position of any
-   entry, it's just a regular token. If it is, then the function sees its about
-   to receive a reader macro.
+Now, the readtable has two constraints:
 
-   Now, the readtable has two constraints:
+* No reader macro's bytes can be a substring of another's. That is, two entries
+  like '#{' and '#' can't exist in the same readtable, because the reader has no
+  way to differentiate between the two. That is, all entries that start with a
+  given byte must have the same length.
+* Reader macros are sorted by character value of its bytes.
 
-     - No reader macro's bytes can be a substring of another's. That is, two
-       entries like '#{' and '#' can't exist in the same readtable, because the
-       reader has no way to differentiate between the two. That is, all entries
-       that start with a given byte must have the same length.
-     - Reader macros are sorted by character value of its bytes.
+This helps search a lot. For one, if the reader is reading a macro that starts
+with a given character % and there are three reader macros that start with that
+character in the entry, the reader will find the first, and then
 
-   This helps search a lot. For one, if the reader is reading a macro that
-   starts with a given character % and there are three reader macros that start
-   with that character in the entry, the reader will find the first, and then
-   will search starting from that position when reading subsequent bytes.
+The matching algorithm is as follows:
 
-   The matching algorithm is as follows:
+* Read the first byte, and having found it in the readtable, record the position
+  of the first matching reader macro in the table as 'pos'.
 
-     - Read the first byte, and having found it in the readtable, record the
-   position of the first matching reader macro in the table as 'pos'.
+* Record the length of that first macro's match string (And, because of the
+  constraint that all macros have the same length, the length of all subsequent
+  macros) as 'len'.
 
-     - Record the length of that first macro's match string (And, because of the
-       constraint that all macros have the same length, the length of all
-       subsequent macros) as 'len'.
+* If 'len' equals 1, then the macro has been found, and its associated reader
+  function is called.
 
-     - If 'len' equals 1, then the macro has been found, and its associated
-       reader function is called.
+* Otherwise, 'len' bytes are read from the input.
 
-     - Otherwise, 'len' bytes are read from the input.
+* If the stream ends before all bytes are read, or the bytes don't match any
+  reader macro, an error is signalled.
 
-     - If the stream ends before all bytes are read, or the bytes don't match
-       any reader macro, an error is signalled.
+* Otherwise, the associated reader macro function of the matched macro is
+  called.
 
-     - Otherwise, the associated reader macro function of the matched macro is
-       called.
-
-  The S-Expression returned by the reader macro function is then recorded by the
-  reader, unless it is NULL, in which case an error is signaled. */
+The S-Expression returned by the reader macro function is then recorded by the
+reader, unless it is NULL, in which case an error is signaled.
+*/
 
 /* Utilities */
 
@@ -95,9 +96,8 @@ struct ReadTable {
    byte. */
 bool completeToken(std::string tok_text);
 
-/* readStream is roughly built along the lines of the Common Lisp reader.
-
-   See: http://www.lispworks.com/documentation/lw51/CLHS/Body/02_b.htm */
+/* For information on the reader algorithm, check the Reader chapter of the
+documentation. */
 SExp* readStream(FILE* stream);
 
 /* A simple function to facilitate reading delimited sequences. It is used to
