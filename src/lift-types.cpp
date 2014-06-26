0765885b71fe994b9d85892c1969fbf7fd997371
Fernando Borretti <eudoxiahp@gmail.com>
2014-06-25 22:55:34 -0300

    Parse type definitions

--- src/lift-types.cpp
+++ src/lift-types.cpp
@@ -3,8 +3,12 @@
 void findTypes(SExp* sexp, TypeEnv* tenv) {
   if(listp(sexp)) {
     if(atomp(first(sexp)) && atomeq(first(sexp), "type")) {
-      /* Is the first element of the list the identifier "type"?  If so, parse the
-         type definition. */
+      /* Parse the type definition. */
+      const char* name = val(first(rest(sexp)));
+      Type def = emitType(rest(rest(sexp)), tenv);
+      tenv->types.find(name)->second = TypeDef(def, "");
+      /* TODO: Validation, docstring */
+      sexp = makeSExp("true", IDENTIFIER);
     } else {
       /* Otherwise, loop over the elements of the list, finding
          their types. */
