bf13892ba53c8076355e3699ad4b0075c226f3a4
Fernando Borretti <eudoxiahp@gmail.com>
2014-07-01 19:51:23 -0300

    Modularize the Rust AST

--- src/ast.rs
+++ src/ast.rs
@@ -1,54 +1,47 @@
-enum AtomValue {
-    Integer(i64),
-    Real(f64),
-    Str(String),
-    Ident(String)
-}
-
-enum SExp {
-    Atom(i64, i64, AtomValue),
-    List(Vec<SExp>),
-    Nil
-}
-
-fn printAtom(val: AtomValue) -> String {
-    match val {
-        Integer(int) => int.to_str(),
-        Real(real) => real.to_str(),
-        Str(str) => str,
-        Ident(str) => str
+mod ast {
+    pub enum AtomValue {
+        Integer(i64),
+        Real(f64),
+        Str(String),
+        Ident(String)
     }
-}
 
-fn printList(list : Vec<SExp>) -> String {
-    let mut out = String::from_str("(");
-    for elem in list.move_iter() {
-        out = format!("{} {}", out, print(elem));
+    pub enum SExp {
+        Atom(i64, i64, AtomValue),
+        List(Vec<SExp>),
+        Nil
     }
-    format!("{})", out)
-}
 
-fn print(sexp : SExp) -> String {
-    match sexp {
-        Atom(_, _, val) => printAtom(val),
-        List(list) => printList(list),
-        Nil => String::from_str("()")
+    fn printAtom(val: AtomValue) -> String {
+        match val {
+            Integer(int) => int.to_str(),
+            Real(real) => real.to_str(),
+            Str(str) => str,
+            Ident(str) => str
+        }
     }
-}
 
-fn classify(str: String) -> AtomValue {
-    Ident(str)
-}
+    fn printList(list : Vec<SExp>) -> String {
+        let mut out = String::from_str("(");
+        for elem in list.move_iter() {
+            out = format!("{} {}", out, print(elem));
+        }
+        format!("{})", out)
+    }
 
-fn atomFromStr(str : String, line: i64, col: i64) -> SExp {
-    Atom(line, col, classify(str))
-}
+    pub fn print(sexp : SExp) -> String {
+        match sexp {
+            Atom(_, _, val) => printAtom(val),
+            List(list) => printList(list),
+            Nil => String::from_str("()")
+        }
+    }
 
-fn main() {
-    let a = Atom(0, 0, Integer(12));
-    let b = Atom(0, 0, Real(3.14));
-    let c = Atom(0, 0, Ident(String::from_str("derp")));
-    println!("{}", print(Nil));
-    println!("{}", print(a));
-    println!("{}", print(List(vec![b, c])));
+    pub fn classify(str: String) -> AtomValue {
+        Ident(str)
+    }
+
+    pub fn atomFromStr(str : String, line: i64, col: i64) -> SExp {
+        Atom(line, col, classify(str))
+    }
 }
