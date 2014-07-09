5f647f7c3a05d8cf003cef77a2b4113dcdc2546f
Fernando Borretti <eudoxiahp@gmail.com>
2014-07-09 17:07:59 -0300

    Add some definitions to annot.rs

--- src/annot.rs
+++ src/annot.rs
@@ -1,2 +1,26 @@
 extern crate ast;
 
+use ast::SExp;
+
+pub struct Definitions {
+    defs: Vec<(String, AST)>
+}
+
+pub enum AST {
+    Let(Definitions),
+    Call(Box<AST>, Vec<Box<AST>>),
+    Atom(i64, i64, ast::AtomValue),
+    Nil 
+}
+
+fn annotate_list(elems: Vec<SExp>) -> AST {
+    Nil
+}
+
+fn annotate(sexp: SExp) -> AST {
+    match sexp {
+        ast::Atom(l, c, v) => Atom(l, c, v),
+        ast::List(vec) => annotate_list(vec),
+        ast::Nil => Nil
+    }
+}
