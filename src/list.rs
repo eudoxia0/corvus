d44d39510dc6a891fd8468669c55fba8eb152445
Fernando Borretti <eudoxiahp@gmail.com>
2014-07-10 20:09:53 -0300

    Add length, append, reverse functions to list.rs

--- src/list.rs
+++ src/list.rs
@@ -34,3 +34,29 @@ pub fn assoc<T: Eq, U>(key: T, list: List<(T, U)>) -> Option<U> {
         _ => None
     }
 }
+
+pub fn length<a>(list: List<a>) -> i64 {
+    let mut length = 0;
+    mapcar(list, |elem| { length = length+1; Nil });
+    length
+}
+
+pub fn append<a>(left: List<a>, right: List<a>) -> List<a> {
+    match left {
+        Nil => right,
+        Value(val) => Nil,
+        Cons(first,rest) => {
+            Nil
+        }
+    }
+}
+
+pub fn reverse<a>(list: List<a>) -> List<a> {
+    match list {
+        Cons(first, rest) => {
+            append(reverse(*rest),
+                   Cons(first, box Nil))
+        },
+        _ => Nil
+    }
+}
