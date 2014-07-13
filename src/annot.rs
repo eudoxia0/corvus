f38601c6956c33ea193b339fffb06b97496d5277
Fernando Borretti <eudoxiahp@gmail.com>
2014-07-13 20:45:16 -0300

    Drop some stuff

--- src/annot.rs
+++ src/annot.rs
@@ -1,26 +1,3 @@
+extern crate list;
 extern crate ast;
 
-use ast::SExp;
-
-pub struct Definitions {
-    defs: Vec<(String, AST)>
-}
-
-pub enum AST {
-    Let(Definitions),
-    Call(Box<AST>, Vec<Box<AST>>),
-    Atom(i64, i64, ast::AtomValue),
-    Nil 
-}
-
-fn annotate_list(first: SExp, rest: SExp) -> AST {
-    Nil
-}
-
-fn annotate(sexp: SExp) -> AST {
-    match sexp {
-        ast::Atom(l, c, v) => Atom(l, c, v),
-        ast::Cons(first, rest) => annotate_list(*first, *rest),
-        ast::Nil => Nil
-    }
-}
