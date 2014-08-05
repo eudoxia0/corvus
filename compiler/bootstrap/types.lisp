1565ff87583304571b1749b00e4f116f75394c9f
Fernando Borretti <eudoxiahp@gmail.com>
2014-08-05 17:56:34 -0300

    Add some type classes

--- compiler/bootstrap/types.lisp
+++ compiler/bootstrap/types.lisp
@@ -2,3 +2,25 @@
 (defpackage :corvus.types
   (:use :cl))
 (in-package :corvus.types)
+
+(defclass <type> () ())
+
+;;;; Scalar types
+
+;;; Integer types
+
+(defclass <integer> (<type>) ())
+
+(defclass <i8> (<integer>) ())
+(defclass <i16> (<integer>) ())
+(defclass <i32> (<integer>) ())
+(defclass <i64> (<integer>) ())
+(defclass <i128> (<integer>) ())
+
+;;; Floating-point types
+
+(defclass <float> (<type>) ())
+
+(defclass <single> (<float>) ())
+(defclass <double> (<float>) ())
+(defclass <quad> (<float>) ())
