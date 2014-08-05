ac60b5a52299858e71d08320cbd9b8e27351ba62
Fernando Borretti <eudoxiahp@gmail.com>
2014-08-05 17:49:27 -0300

    Add some more parser tests

--- compiler/bootstrap/tests/parser.lisp
+++ compiler/bootstrap/tests/parser.lisp
@@ -14,4 +14,18 @@
   (is-true (typep (parse-string "3test3") '<identifier>))
   (is-true (typep (parse-string "test-test-test") '<identifier>)))
 
+(test integer
+  (is-true (typep (parse-string "1") '<integer>))
+  (is-true (typep (parse-string "1234") '<integer>))
+  (is-true (typep (parse-string "+213213") '<integer>))
+  (is-true (typep (parse-string "-23232") '<integer>)))
+
+(test float
+  (is-true (typep (parse-string "23.0") '<float>))
+  (is-true (typep (parse-string "3.14") '<float>))
+  (is-true (typep (parse-string "+3.14") '<float>))
+  (is-true (typep (parse-string "-3.14") '<float>))
+  (is-true (typep (parse-string "45e10") '<float>))
+  (is-true (typep (parse-string "-10e2") '<float>)))
+
 (run! 'parser)
