152a4600dc90b3510e8135e6c4bfceed6fdc4fdc
Fernando Borretti <eudoxiahp@gmail.com>
2014-08-05 17:58:13 -0300

    Add the width method

--- compiler/bootstrap/types.lisp
+++ compiler/bootstrap/types.lisp
@@ -17,6 +17,13 @@
 (defclass <i64> (<integer>) ())
 (defclass <i128> (<integer>) ())
 
+(defgeneric width (type)
+  (:method ((int <i8>)) 8)
+  (:method ((int <i16>)) 16)
+  (:method ((int <i32>)) 32)
+  (:method ((int <i64>)) 64)
+  (:method ((int <i128>)) 128))
+
 ;;; Floating-point types
 
 (defclass <float> (<type>) ())
@@ -24,3 +31,8 @@
 (defclass <single> (<float>) ())
 (defclass <double> (<float>) ())
 (defclass <quad> (<float>) ())
+
+(defgeneric width (type)
+  (:method ((float <single>)) 32)
+  (:method ((float <double>)) 64)
+  (:method ((float <quad>)) 128))
