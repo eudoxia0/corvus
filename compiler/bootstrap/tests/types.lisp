52f75adba1fd523092b710207d1d141c1320866d
Fernando Borretti <eudoxiahp@gmail.com>
2014-08-06 18:34:31 -0300

    Add some more tests of emit-type

--- compiler/bootstrap/tests/types.lisp
+++ compiler/bootstrap/tests/types.lisp
@@ -1,17 +1,26 @@
 (in-package :cl-user)
 (defpackage :corvus-test.types
-  (:use :cl :fiveam :corvus.types))
+  (:use :cl :fiveam :corvus.types)
+  (:import-from :corvus.parser
+                :parse-string))
 (in-package :corvus-test.types)
 
 (def-suite types)
 (in-suite types)
 
+(defun emit-parsed (string)
+  (emit-type (parse-string string) (create-default-tenv)))
+
 (test emit-atom
-  (is-true (typep (emit-type nil (create-default-tenv))
-                  '<unit>))
-  (is-true (typep (emit-type (make-instance 'corvus.parser:<atom>
-                                            :val "bool")
-                             (create-default-tenv))
-                  '<bool>)))
+  (is-true (typep (emit-parsed "()") '<unit>))
+  (is-true (typep (emit-parsed "bool") '<bool>)))
+
+(test emit-tuple
+  (is-true (typep (emit-parsed "(tup i8 i8 i8)") '<tuple>))
+  (is-true (typep (emit-parsed "{i8 i16 i32}") '<tuple>)))
+
+(test emit-array
+  (is-true (typep (emit-parsed "(array i8)") '<array>))
+  (is-true (typep (emit-parsed "[i8]") '<array>)))
 
 (run! 'types)
