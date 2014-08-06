b52565b3f98002d67d8fac903ca5a15b0325c2db
Fernando Borretti <eudoxiahp@gmail.com>
2014-08-06 18:07:23 -0300

    Expand the module system

--- compiler/bootstrap/modules.lisp
+++ compiler/bootstrap/modules.lisp
@@ -3,12 +3,16 @@
   (:use :cl :corvus.util))
 (in-package :corvus.modules)
 
+;;; Data structures
+
 (defclass <module> ()
   ((imports :initarg :imports :reader imports)
    (exports :initarg :exports :reader exports)))
 
 (defclass <module-env> ()
   ((modules :initarg :modules :accessor modules)))
+
+;;; Module definition
 
 (defun find-subform (tree name)
   (declare (type <sexp> tree)
@@ -21,3 +25,24 @@
 
 (defun process-module-definitions (tree env)
   nil)
+
+;;; Appending module prefixes to an AST
+
+(defun has-prefix (string)
+  "Return `T` if the string has module prefix."
+  (declare (type string string))
+  (if (position #\: string) t))
+
+(defun add-prefix (atom prefix)
+  (make-instance '<atom>
+                 :val (concatenate 'string prefix ":" (val atom))))
+
+(defun modularize-atom (atom current-prefix)
+  (declare (type <atom> atom)
+           (type string current-prefix))
+  (if (and (typep atom '<identifier>)
+           (not (has-prefix (val atom))))
+      ;; No prefix, add that of the current module
+      (add-prefix atom current-prefix))
+      ;; If it has a prefix or isn't an identifier, leave it as-is
+      atom)
