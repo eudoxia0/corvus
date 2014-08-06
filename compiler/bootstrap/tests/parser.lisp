d1ef33fce219dd441121aea1fe8eb25417137581
Fernando Borretti <eudoxiahp@gmail.com>
2014-08-06 17:26:28 -0300

    Add tests of array, tuple literals

--- compiler/bootstrap/tests/parser.lisp
+++ compiler/bootstrap/tests/parser.lisp
@@ -50,4 +50,18 @@
   (is (equal (tree-to-string (parse-string "(if cond (f 1) (g 2))"))
              (list "if" "cond" (list "f" "1") (list "g" "2")))))
 
+(test array
+  (is (equal (tree-to-string (parse-string "[1 2 3]"))
+             (list "array" "1" "2" "3")))
+  (is (equal (tree-to-string (parse-string "[[1 2 3][4 5 6]]"))
+             (list "array"
+                   (list "array" "1" "2" "3")
+                   (list "array" "4" "5" "6")))))
+
+(test tuple
+  (is (equal (tree-to-string (parse-string "{0 0 0}"))
+             (list "tup" "0" "0" "0")))
+  (is (equal (tree-to-string (parse-string "{1 3.14 \"test\"}"))
+             (list "tup" "1" "3.14" "test"))))
+
 (run! 'parser)
