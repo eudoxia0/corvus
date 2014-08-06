4cb81ac5f446341b4ac3b379375ccdd987f416c8
Fernando Borretti <eudoxiahp@gmail.com>
2014-08-06 17:49:19 -0300

    Import some utilities from cmacro's pattern matcher

--- compiler/bootstrap/macros.lisp
+++ compiler/bootstrap/macros.lisp
@@ -3,6 +3,8 @@
   (:use :cl :trivial-types :corvus.parser))
 (in-package :corvus.macros)
 
+;;; Data structures
+
 (defclass <macro-case> ()
   ((pattern :initarg :pattern
             :reader pattern
@@ -22,6 +24,8 @@
            :type hash-table
            :initform (make-hash-table :test #'equal))))
 
+;;; Macro definition
+
 (defun process-cases (tree)
   (declare (type <sexp> tree))
   (loop for (pattern template) on tree by #'cddr collecting
@@ -35,6 +39,28 @@
                  :cases (process-cases tree)))
 
 (defun define-macro (tree menv)
+  (declare (type <sexp> tree)
+           (type <macro-env> menv))
   (let ((name (val (first tree))))
     (setf (gethash name (macros menv))
           (parse-macro-definition (rest tree)))))
+
+;;; Pattern matching
+
+;; The environment
+
+(defun env-append (pattern input env)
+  (append env (list (list pattern input))))
+
+(defun env-append-rest (pattern input env)
+  (env-append pattern (list :rest
+                            (if (atom input) (list input) input)) env))
+
+;; Utilities
+
+(defun var-p (string)
+  (declare (type string string))
+  (if (> (length string) 0)
+      (char= (elt string 0) #\$)))
+
+;;; Macroexpansion
