a8045e09aa2a64f090a7160846524a98b6891c54
Fernando Borretti <eudoxiahp@gmail.com>
2014-08-06 17:40:53 -0300

    Add a few functions for defining macros

--- compiler/bootstrap/macros.lisp
+++ compiler/bootstrap/macros.lisp
@@ -21,3 +21,20 @@
            :reader macros
            :type hash-table
            :initform (make-hash-table :test #'equal))))
+
+(defun process-cases (tree)
+  (declare (type <sexp> tree))
+  (loop for (pattern template) on tree by #'cddr collecting
+    (make-instance '<macro-case>
+                   :pattern pattern
+                   :template template)))
+
+(defun parse-macro-definition (tree)
+  (declare (type <sexp> tree))
+  (make-instance '<macro>
+                 :cases (process-cases tree)))
+
+(defun define-macro (tree menv)
+  (let ((name (val (first tree))))
+    (setf (gethash name (macros menv))
+          (parse-macro-definition (rest tree)))))
