fcefffa8caa6535dedf38859b3f120158e622af5
Fernando Borretti <eudoxiahp@gmail.com>
2014-08-06 18:45:41 -0300

    Add some tests of parsing type fields

--- compiler/bootstrap/tests/types.lisp
+++ compiler/bootstrap/tests/types.lisp
@@ -15,12 +15,24 @@
   (is-true (typep (emit-parsed "()") '<unit>))
   (is-true (typep (emit-parsed "bool") '<bool>)))
 
+(test emit-array
+  (is-true (typep (emit-parsed "(array i8)") '<array>))
+  (is-true (typep (emit-parsed "[i8]") '<array>)))
+
 (test emit-tuple
   (is-true (typep (emit-parsed "(tup i8 i8 i8)") '<tuple>))
   (is-true (typep (emit-parsed "{i8 i16 i32}") '<tuple>)))
 
-(test emit-array
-  (is-true (typep (emit-parsed "(array i8)") '<array>))
-  (is-true (typep (emit-parsed "[i8]") '<array>)))
+(test emit-record
+  (is-true (typep (corvus.types::parse-type-field (parse-string "(name bool)")
+                                                  (create-default-tenv))
+                  '<field>))
+  (is (equal (name (corvus.types::parse-type-field (parse-string "(name bool)")
+                                                  (create-default-tenv)))
+             "name"))
+  (is-true (typep (base-type (corvus.types::parse-type-field
+                              (parse-string "(name bool)")
+                              (create-default-tenv)))
+                  '<bool>)))
 
 (run! 'types)
