0cc85b1f83e4dd29a64b1a522d529b4b2b26f295
Fernando Borretti <eudoxiahp@gmail.com>
2014-08-06 17:31:44 -0300

    Expand macros.lisp

--- compiler/bootstrap/macros.lisp
+++ compiler/bootstrap/macros.lisp
@@ -1,4 +1,23 @@
 (in-package :cl-user)
 (defpackage :corvus.macros
-  (:use :cl))
+  (:use :cl :trivial-types :corvus.parser))
 (in-package :corvus.macros)
+
+(defclass <macro-case> ()
+  ((pattern :initarg :pattern
+            :reader pattern
+            :type <sexp>)
+   (template :initarg :template
+             :reader template
+             :type <sexp>)))
+
+(defclass <macro> ()
+  ((cases :initarg :cases
+          :reader cases
+          :type (proper-list <macro-case>))))
+
+(defclass <macro-env> ()
+  ((macros :initarg :macros
+           :reader macros
+           :type hash-table
+           :initform (make-hash-table :test #'equal))))
