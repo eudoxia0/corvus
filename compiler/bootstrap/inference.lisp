065e3df55e58fca80230c9d5016a6648a475d05f
Fernando Borretti <eudoxiahp@gmail.com>
2014-08-08 18:06:29 -0300

    Port type variable code

--- compiler/bootstrap/inference.lisp
+++ compiler/bootstrap/inference.lisp
@@ -5,3 +5,20 @@
 ;;;; http://www.cs.cmu.edu/afs/cs/Web/Groups/AI/lang/scheme/code/ext/types/polytype/polytype.scm
 
 (in-package :corvus.types)
+
+;;; Type variables
+
+(defparameter *var-count* 0
+  "The ID of variables.")
+
+(defclass <tvar> ()
+  ((n :initarg :n :reader n :type integer :initform (incf *var-count*))))
+
+(defun new-var ()
+  (make-instance '<tvar>))
+
+(defmethod print-object ((var <tvar>) stream)
+  (format stream "?~A" (n var)))
+
+(defmethod variable< ((x <tvar>) (y <tvar>))
+  (< (n x) (n y)))
