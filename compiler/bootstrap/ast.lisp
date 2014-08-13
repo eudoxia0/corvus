5619b293ba023923d52faf6d6d64ef50986e0648
Fernando Borretti <eudoxiahp@gmail.com>
2014-08-13 17:08:12 -0300

    Add AST definitions

--- compiler/bootstrap/ast.lisp
+++ compiler/bootstrap/ast.lisp
@@ -1,18 +1,17 @@
 (in-package :cl-user)
 (defpackage :corvus.ast
   (:use :cl :trivial-types)
+  (:import-from :corvus.parser
+                :ident-equal)
   (:export :annotate))
 (in-package :corvus.ast)
 
 ;;; Built-in syntactic sugar
 
-(defun desugar-body (body)
-  "Take a list of expressions. If it has more than one expr, turn it into a
-'begin' statement."
-  (if (> (length body) 1)
-      (cons (make-instance 'corvus.parser:<identifier>
-                           :val "begin")
-            body)))
+(defun make-begin-expression (body)
+  (cons (make-instance 'corvus.parser:<identifier>
+                       :val "begin")
+        body))
 
 (defun desugar-bodies (expr)
   "Recur through the expression, desugaring the bodies of 'let's and 'lambda'."
@@ -23,7 +22,12 @@
         (cond
           ((or (ident-equal first "let") (ident-equal first "lambda"))
            ;; 'let' and 'lambda' have the same structure
-           (list first (second expr) (desugar-body (rest args))))
+           (let ((body (rest args)))
+             (list first
+                   (second expr)
+                   (if (> (length body) 1)
+                       (make-begin-expression body)
+                       body))))
           (t
            (cons (desugar-bodies (first expr))
                  (desugar-bodies (rest expr))))))))
@@ -39,3 +43,39 @@
   * The body of a 'let' or 'lambda' expression is converted into a 'begin'
     expression."
   (desugar-bodies expr))
+
+;;; AST definitions
+
+(defclass <sexp> () ())
+
+(defclass <atom> (<sexp>)
+  ((val :initarg :val :reader val :type string)
+   (line :initarg :line :reader line :initform 0)
+   (col :initarg :col :reader col :initform 0)))
+
+(defmethod print-object ((atom <atom>) stream)
+  (format stream "~A" (val atom)))
+
+(defclass <identifier> (<atom>) ())
+(defclass <constant> (<atom>) ())
+(defclass <integer> (<constant>) ())
+(defclass <float> (<constant>) ())
+(defclass <string> (<constant>) ())
+
+(defclass <list> (<sexp>) ())
+
+(defclass <begin> (<list>)
+  ((steps :initarg :steps
+          :reader steps
+          :type (proper-list <ast>))))
+
+(defclass <let> (<list>)
+  ((var :initarg :var
+        :reader var
+        :type <identifier>)
+   (val :initarg :val
+        :reader val
+        :type <ast>)
+   (body :initarg :body
+         :reader body
+         :type <ast>)))
